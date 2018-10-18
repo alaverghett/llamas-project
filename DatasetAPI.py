b1754fa7ae8889c1c219489f95bb5844a96be68f
Abdel Feliciano <abdel@mail.usf.edu>
2018-10-18 10:36:09 -0400

    Changed how reviews get generated

--- DatasetAPI.py
+++ DatasetAPI.py
@@ -9,33 +9,44 @@ class Dataset():
         '''
         Expects a dataset directory with yelp_business_new.json and yelp_review_new.json
         '''
-        BUSINESS_DATASET_NEW = "dataset/yelp_business_new.json"
-        REVIEW_DATASET_NEW = "dataset/yelp_review_minified.json"
-        self.business_file = open(BUSINESS_DATASET_NEW)
-        self.review_file = open(REVIEW_DATASET_NEW)
+        self.BUSINESS_DATASET_NEW = "dataset/yelp_business_new.json"
+        self.REVIEW_DATASET_NEW = "dataset/yelp_review_minified.json"
+        self.BUSINESS_DATASET = "./dataset/yelp_academic_dataset_business.json"
+        self.REVIEW_DATASET = "./dataset/yelp_academic_dataset_review.json"
+        # self.business_file = open(BUSINESS_DATASET_NEW)
+        # self.review_file = open(REVIEW_DATASET_NEW)
 
     def get_business_ids(self):
-        return (business['business_id'] for business in ijson.items(self.business_file, 'item'))
+        return (json.loads(business)['business_id'] for business in open(self.BUSINESS_DATASET, 'r'))
 
     def get_reviews(self, business_id):
         '''
         Returns generator
         Slower in traversal
         '''
-        return (review for review in ijson.items(self.review_file, 'item') if review['business_id'] == business_id)
+        with open(self.REVIEW_DATASET_NEW) as reviews_file:
+            for text in reviews_file:
+                if text.strip('\n') in ['[', ',', ']']:
+                    continue
+
+                review = json.loads(text)
+                if review['business_id'] == business_id:
+                    yield review
+        # return (review for review in ijson.items(self.review_file, 'item') if review['business_id'] == business_id)
 
     def get_reviews_std(self, business_id):
         '''
+        DEPRECATED: DO NOT USE
         Returns a list
         Uses standard json module
         Big overhead to load data
         '''
-
-        print("Loading")
-        reviews = json.load(self.review_file)
-        out = [review for review in reviews if review['business_id'] == business_id]
-        print("Loaded")
-        return out
+        pass
+        # print("Loading")
+        # reviews = json.load(self.review_file)
+        # out = [review for review in reviews if review['business_id'] == business_id]
+        # print("Loaded")
+        # return out
 
 
 if __name__ == '__main__':
@@ -46,11 +57,15 @@ if __name__ == '__main__':
 
     j = i.get_business_ids().__next__()
 
-    start = time.time()
-    end = None
-    for review in i.get_reviews_std(j):
-        end = time.time()
-        print(review)
-        print("Time = " + str(end - start) + " seconds")
-        input()
-        start = time.time()
+    for n in i.get_business_ids():
+        for review in i.get_reviews(n):
+            print(review["business_id"], review["user_id"])
+        pass
+    # start = time.time()
+    # end = None
+    # for review in i.get_reviews_std(j):
+    #     end = time.time()
+    #     print(review)
+    #     print("Time = " + str(end - start) + " seconds")
+    #     input()
+    #     start = time.time()
